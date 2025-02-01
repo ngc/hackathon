cuhacking/2025

feat(db): create data models

libs/db/access/isOrganizer.ts
@@ -1,0 +1,10 @@
+import type { User } from '@cuhacking/db/types/payload-types'
+import type { Access, FieldAccess } from 'payload'
+
+export const isOrganizer: Access<User> = ({ req: { user } }) => {
+  return Boolean(user?.role?.find(role => role.includes('organizer')))
+}
+
+export const isOrganizerFieldLevel: FieldAccess<{ id: string }, User> = ({ req: { user } }) => {
+  return Boolean(user?.role?.find(role => role.includes('organizer')))
+}

libs/db/collections/models/Host-To-Event.ts
@@ -1,0 +1,46 @@
+import type { CollectionConfig } from 'payload'
+
+export const HostToEvent: CollectionConfig = {
+  slug: 'host-to-event',
+  admin: {
+    useAsTitle: 'formattedTitle',
+  },
+  fields: [
+    // Basic Info
+    {
+      name: 'formattedTitle',
+      type: 'text',
+      hooks: {
+        beforeChange: [
+          async ({ data, req }) => {
+            const { event, host } = data as any
+
+            const eventObj = event
+              ? await req.payload.findByID({ collection: 'general-event', id: event })
+              : null
+            const organizationObj = host
+              ? await req.payload.findByID({ collection: 'organization', id: host })
+              : null
+
+            const eventName = eventObj?.formattedTitle || 'Unknown User'
+            const organizationName = organizationObj?.name || 'Unknown Organization'
+
+            return `${eventName} - ${organizationName}`
+          },
+        ],
+      },
+    },
+    {
+      name: 'event',
+      type: 'relationship',
+      relationTo: 'base-event',
+      required: true,
+    },
+    {
+      name: 'host',
+      type: 'relationship',
+      relationTo: 'organization',
+      required: true,
+    },
+  ],
+}

libs/db/collections/models/User-To-Event.ts
@@ -1,0 +1,25 @@
+import type { CollectionConfig } from 'payload'
+
+export const UserToEvent: CollectionConfig = {
+  slug: 'user-to-event',
+  fields: [
+    {
+      name: 'user',
+      type: 'relationship',
+      relationTo: 'user',
+      required: true,
+    },
+    {
+      name: 'event',
+      type: 'relationship',
+      relationTo: 'base-event',
+      required: true,
+    },
+    {
+      name: 'role',
+      type: 'relationship',
+      relationTo: 'event-role',
+      required: true,
+    },
+  ],
+}

ngc/hackathon

libs/db/access/isOrganizer.ts
@@ -1,0 +1,10 @@
+import type { User } from '@cuhacking/db/types/payload-types'
+import type { Access, FieldAccess } from 'payload'
+
+export const isOrganizer: Access<User> = ({ req: { user } }) => {
+  return Boolean(user?.roles?.find(role => role.includes('organizer')))
+}
+
+export const isOrganizerFieldLevel: FieldAccess<{ id: string }, User> = ({ req: { user } }) => {
+  return Boolean(user?.roles?.find(role => role.includes('organizer')))
+}

libs/db/collections/models/Host-To-Event.ts
@@ -1,0 +1,46 @@
+import type { CollectionConfig } from 'payload'
+
+export const HostToEvent: CollectionConfig = {
+  slug: 'host-to-event',
+  admin: {
+    useAsTitle: 'formattedTitle',
+  },
+  fields: [
+    // Basic Info
+    {
+      name: 'formattedTitle',
+      type: 'text',
+      hooks: {
+        beforeChange: [
+          async ({ data, req }) => {
+            const { event, host } = data as any
+
+            const eventObj = event
+              ? await req.payload.findByID({ collection: 'general-event', id: event })
+              : null
+            const organizationObj = host
+              ? await req.payload.findByID({ collection: 'organization', id: host })
+              : null
+
+            const eventName = eventObj?.formattedTitle || 'Unknown User'
+            const organizationName = organizationObj?.name || 'Unknown Organization'
+
+            return `${eventName} - ${organizationName}`
+          },
+        ],
+      },
+    },
+    {
+      name: 'event',
+      type: 'relationship',
+      relationTo: 'general-event',
+      required: true,
+    },
+    {
+      name: 'host',
+      type: 'relationship',
+      relationTo: 'organization',
+      required: true,
+    },
+  ],
+}

libs/db/collections/models/User-To-Event.ts
@@ -1,0 +1,25 @@
+import type { CollectionConfig } from 'payload'
+
+export const UserToEvent: CollectionConfig = {
+  slug: 'user-to-event',
+  fields: [
+    {
+      name: 'user',
+      type: 'relationship',
+      relationTo: 'user',
+      required: true,
+    },
+    {
+      name: 'event',
+      type: 'relationship',
+      relationTo: 'general-event',
+      required: true,
+    },
+    {
+      name: 'role',
+      type: 'relationship',
+      relationTo: 'event-role',
+      required: true,
+    },
+  ],
+}
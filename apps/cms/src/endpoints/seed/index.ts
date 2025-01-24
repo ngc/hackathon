58ddfa895c1534af1acb558dec8616b977b8edf8
HasithDeAlwis <hasithde24@gmail.com>
2025-01-24 01:58:17 -0500

    feat(db): create data models

--- apps/cms/src/endpoints/seed/index.ts
+++ apps/cms/src/endpoints/seed/index.ts
@@ -1,5 +1,5 @@
 import type { Payload, PayloadRequest } from 'payload';
-import { links } from './social-links'
+// import { links } from './social-links'
 
 export const seed = async ({
   payload,
@@ -9,29 +9,5 @@ export const seed = async ({
   req: PayloadRequest;
 }): Promise<void> => {
   payload.logger.info('Seeding social media links...');
-
-  // Clear existing entries in the `social-links` collection
-  payload.logger.info('— Clearing existing links...');
-  await payload.delete({
-    collection: 'social-links',
-    depth: 0,
-    where: {}, // Deletes all entries in the collection
-  });
-
-  // Seed new social media links
-  payload.logger.info('— Adding new links...');
-  // await Promise.all(
-  //   links.map((link) =>
-  //     payload.create({
-  //       collection: 'social-links',
-  //       data: {
-  //         platform: link.label || link.text,
-  //         url: link.url,
-  //         description: link.text,
-  //       },
-  //     }),
-  //   ),
-  // );
-
-  payload.logger.info('Social media links seeded successfully!');
+  console.log(req)
 };
--- apps/portal/env.d.ts
+++ apps/portal/env.d.ts
@@ -1,2 +1,4 @@
 /// <reference types="@remix-run/node" />
 /// <reference types="vite/client" />
+
+// 25a793247f13455697ff1fab69bf89d3a8108413273ca6c0b8cdf62ef056faaa
